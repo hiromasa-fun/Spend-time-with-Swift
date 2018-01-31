907fe992dfa99aa4ba42097cda4ae273e79e75ee
HiormasaKawakita <kawakita.hiromasa.hdsf@gmail.com>
2018-01-31 15:43:55 +0900

    make genreViewController.swift

---
+++ iTunesSongRank/iTunesSongRank/genreViewController.swift
@@ -0,0 +1,35 @@
+//
+//  genreViewController.swift
+//  iTunesSongRank
+//
+//  Created by 川北紘正 on 2018/01/31.
+//  Copyright © 2018年 川北紘正. All rights reserved.
+//
+
+import UIKit
+
+class genreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
