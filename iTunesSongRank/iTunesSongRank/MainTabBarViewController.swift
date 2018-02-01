3b5c0a3fda7c7df5803bef120d81e4d32747e302
HiormasaKawakita <kawakita.hiromasa.hdsf@gmail.com>
2018-02-01 19:40:34 +0900

    create MainTabBarViewController.swift

---
+++ iTunesSongRank/iTunesSongRank/MainTabBarViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainTabBarViewController.swift
+//  iTunesSongRank
+//
+//  Created by 川北紘正 on 2018/02/01.
+//  Copyright © 2018年 川北紘正. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let vc = ViewController()
+        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+        let nv = UINavigationController(rootViewController: vc)
+        
+        let vc2 = genreViewController()
+        vc2.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 2)
+        let nv2 = UINavigationController(rootViewController: vc2)
+        setViewControllers([nv, nv2], animated: false)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
